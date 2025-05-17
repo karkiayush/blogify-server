d509906c9f0f732f371731046d41d5384a83a930
karkiayush <karkiayush619@gmail.com>
2025-05-17 22:24:21 +0545

    Initial commit: Spring Boot project setup with Keycloak integration, PostgreSQL config, and Docker support

---
+++ src/main/java/com/karkiayush/blogifybackend/BlogifyBackendApplication.java
@@ -0,0 +1,11 @@
+package com.karkiayush.blogifybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogifyBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogifyBackendApplication.class, args);
+    }
+}
---
+++ src/test/java/com/karkiayush/blogifybackend/BlogifyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karkiayush.blogifybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogifyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
